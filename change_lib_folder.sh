09d4fd17c638b50789e368aba1fbc59ac50719e0
malifnasrulloh <alifnasrulloh.jbg@gmail.com>
2026-01-14 15:49:56 +0700

    Add custom library folder path to project properties and disable compile on save

--- change_lib_folder.sh
+++ change_lib_folder.sh
@@ -1,9 +1,14 @@
-lib_folder="C:/SIMRS-Khanza-fork/lib/"
+lib_folder="C:/SIMRS-Khanza-fork/lib"
 
-find . -type f -name "project.properties" -exec sed -i "s|/Users/windiartonugroho/Lib/|$lib_folder|g" {} +
-find . -type f -name "project.properties" -exec sed -i "s|/Users/windiartonugroho/lib/|$lib_folder|g" {} +
-find . -type f -name "project.properties" -exec sed -i "s|/Users/windiartonugroho/Documents/lib/|$lib_folder|g" {} +
-find . -type f -name "project.properties" -exec sed -i "s|../../../../lib/|$lib_folder|g" {} +
+# find . -type f -name "project.properties" -exec sed -i "s|/Users/windiartonugroho/Lib/|$lib_folder|g" {} +
+# find . -type f -name "project.properties" -exec sed -i "s|/Users/windiartonugroho/lib/|$lib_folder|g" {} +
+# find . -type f -name "project.properties" -exec sed -i "s|/Users/windiartonugroho/Documents/lib/|$lib_folder|g" {} +
+# find . -type f -name "project.properties" -exec sed -i "s|../../../../lib/|$lib_folder|g" {} +
+
+find . -type f -name "project.properties" \
+  -exec sed -i "s|^custom_lib_folder=.*|custom_lib_folder=${lib_folder}|" {} +
+
+
 
 
 dist_folder="C:/KhanzaHMSWindowsAlip/"
@@ -14,3 +19,6 @@ find . -type f -name "project.properties" -exec sed -i "s|dist.dir=dist|dist.dir
 
 find . -name "project.properties" -exec sed -i '/file.reference.commons-codec-1.12.jar/d' {} +
 
+find . -name "private.properties" \
+  -exec grep -q "compile.on.save" {} \; \
+  -exec sed -i 's/^compile\.on\.save=.*/compile.on.save=false/' {} +
