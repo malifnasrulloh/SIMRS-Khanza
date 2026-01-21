e3cc1b58299576f49e39505a2e8c7f175c73acb2
malifnasrulloh <alifnasrulloh.jbg@gmail.com>
2026-01-21 17:54:54 +0700

    refactor: update EnkripsiAES class to use constants and improve code readability

--- KhanzaSecurity16bit/src/AESsecurity/EnkripsiAES.java
+++ KhanzaSecurity16bit/src/AESsecurity/EnkripsiAES.java
@@ -5,11 +5,11 @@
  */
 package AESsecurity;
 
+import java.nio.charset.StandardCharsets;
 import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import org.apache.commons.codec.binary.Base64;
-
 
 /**
  *
@@ -17,32 +17,34 @@ import org.apache.commons.codec.binary.Base64;
  */
 public class EnkripsiAES {
 
-    private static final String key = "Bar12345Bar12345"; // 128 bit key
-    private static final String initVector = "sayangsamakhanza"; // 16 bytes IV
+    private static final String KEY = "Bar12345Bar12345"; // 128 bit key
+    private static final String INITVECTOR = "sayangsamakhanza"; // 16 bytes IV
 
     public static String decrypt(String encrypted) {
         try {
-            IvParameterSpec iv = new IvParameterSpec(initVector.getBytes("UTF-8"));
-            SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
+            IvParameterSpec iv = new IvParameterSpec(INITVECTOR.getBytes(StandardCharsets.UTF_8));
+            SecretKeySpec skeySpec = new SecretKeySpec(KEY.getBytes(StandardCharsets.UTF_8), "AES");
 
-            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
             cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
 
-            byte[] original = Base64.decodeBase64(encrypted);
-            return new String(cipher.doFinal(original));
+            byte[] decoded = Base64.decodeBase64(encrypted);
+            return new String(cipher.doFinal(decoded), StandardCharsets.UTF_8);
         } catch (Exception ex) {
+            ex.printStackTrace();
+            System.out.println(ex);
             System.out.println("Ciluk Baaaaaaaa!!!!!");
         }
 
         return null;
     }
-    
+
     public static String encrypt(String value) {
         try {
-            IvParameterSpec iv = new IvParameterSpec(initVector.getBytes("UTF-8"));
-            SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
+            IvParameterSpec iv = new IvParameterSpec(INITVECTOR.getBytes(StandardCharsets.UTF_8));
+            SecretKeySpec skeySpec = new SecretKeySpec(KEY.getBytes(StandardCharsets.UTF_8), "AES");
 
-            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
             cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
 
             byte[] encrypted = cipher.doFinal(value.getBytes());
@@ -50,8 +52,6 @@ public class EnkripsiAES {
         } catch (Exception ex) {
             System.out.println("Ciluk Baaaaaaaa!!!!!");
         }
-
         return null;
     }
-    
 }
