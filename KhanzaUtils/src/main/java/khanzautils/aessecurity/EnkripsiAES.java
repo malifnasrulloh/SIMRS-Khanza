eb47d82e9162cf69d07338081c2d685035973834
malifnasrulloh <alifnasrulloh.jbg@gmail.com>
2026-04-13 06:53:10 +0700

    feat: sync with webapps/conf/conf.php encryption

--- KhanzaUtils/src/main/java/khanzautils/aessecurity/EnkripsiAES.java
+++ KhanzaUtils/src/main/java/khanzautils/aessecurity/EnkripsiAES.java
@@ -6,6 +6,8 @@
 package khanzautils.aessecurity;
 
 import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.util.Arrays;
 import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
@@ -53,5 +55,45 @@ public class EnkripsiAES {
             System.out.println("Ciluk Baaaaaaaa!!!!!");
         }
         return null;
+    }
+    
+        public static String encrypt_decrypt(String value, String action) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+
+            String keyHex = bytesToHex(digest.digest(KEY.getBytes(StandardCharsets.UTF_8)));
+            String ivHex = bytesToHex(digest.digest(INITVECTOR.getBytes(StandardCharsets.UTF_8)));
+
+            SecretKeySpec skeySpec = new SecretKeySpec(Arrays.copyOf(keyHex.getBytes(StandardCharsets.UTF_8), 32), "AES");
+            IvParameterSpec iv = new IvParameterSpec(Arrays.copyOf(ivHex.getBytes(StandardCharsets.UTF_8), 16));
+
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            cipher.init(action.equalsIgnoreCase("e") ? Cipher.ENCRYPT_MODE : Cipher.DECRYPT_MODE, skeySpec, iv);
+
+            if (action.equalsIgnoreCase("e")) {
+                String encoded1 = Base64.encodeBase64String(cipher.doFinal(value.getBytes(StandardCharsets.UTF_8)));
+                return Base64.encodeBase64String(encoded1.getBytes(StandardCharsets.UTF_8));
+            } else {
+                byte[] decoded1 = Base64.decodeBase64(value);
+                byte[] decoded2 = Base64.decodeBase64(new String(decoded1, StandardCharsets.UTF_8));
+                return new String(cipher.doFinal(decoded2), StandardCharsets.UTF_8);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            System.out.println("Ciluk Baaaaaaaa!!!!!");
+        }
+        return null;
+    }
+
+    private static String bytesToHex(byte[] hash) {
+        StringBuilder hexString = new StringBuilder();
+        for (byte b : hash) {
+            String hex = Integer.toHexString(0xff & b);
+            if (hex.length() == 1) {
+                hexString.append('0');
+            }
+            hexString.append(hex);
+        }
+        return hexString.toString();
     }
 }
