1bb6d3c03750e7828792ce918018e99d26c0548c
malifnasrulloh <alifnasrulloh.jbg@gmail.com>
2026-06-07 22:30:35 +0700

    feat(bridging): add Z-code filters to Neonate, CNC, and da types in Java client

--- src/bridging/EpisodeOfCareType.java
+++ src/bridging/EpisodeOfCareType.java
@@ -16,6 +16,72 @@ public enum EpisodeOfCareType {
             "TB-SO",
             "Tuberkulosis Sensitif Obat",
             new String[]{"%A15%", "%A16%", "%A17%", "%A18%", "%A19%"}
+    ),
+    TB_RO(
+            "http://terminology.kemkes.go.id/CodeSystem/episodeofcare-type",
+            "TB-RO",
+            "Tuberkulosis Resisten Obat",
+            new String[]{}
+    ),
+    Neonate(
+            "http://terminology.kemkes.go.id/CodeSystem/episodeofcare-type",
+            "Neonate",
+            "Neonate",
+            new String[]{"%P%", "%Z38%"}
+    ),
+    CKD(
+            "http://terminology.kemkes.go.id/CodeSystem/episodeofcare-type",
+            "CKD",
+            "Chronic Kidney Disease",
+            new String[]{"%N18%"}
+    ),
+    CNC(
+            "http://terminology.kemkes.go.id/CodeSystem/episodeofcare-type",
+            "CNC",
+            "Cancer Management Care",
+            new String[]{"%C%", "%D0%", "%D1%", "%D2%", "%D3%", "%D4%", "%Z51.1%", "%Z51.0%"}
+    ),
+    CAD(
+            "http://terminology.kemkes.go.id/CodeSystem/episodeofcare-type",
+            "CAD",
+            "Coronary Arterial Disease",
+            new String[]{"%I20%", "%I21%", "%I22%", "%I23%", "%I24%", "%I25%"}
+    ),
+    CVD(
+            "http://terminology.kemkes.go.id/CodeSystem/episodeofcare-type",
+            "CVD",
+            "Cerebrovascular Disease",
+            new String[]{"%I60%", "%I61%", "%I62%", "%I63%", "%I64%", "%I65%", "%I66%", "%I67%", "%I68%", "%I69%"}
+    ),
+    hacc(
+            "http://terminology.hl7.org/CodeSystem/episodeofcare-type",
+            "hacc",
+            "Home and Community Care",
+            new String[]{}
+    ),
+    pac(
+            "http://terminology.hl7.org/CodeSystem/episodeofcare-type",
+            "pac",
+            "Post Acute Care",
+            new String[]{}
+    ),
+    diab(
+            "http://terminology.hl7.org/CodeSystem/episodeofcare-type",
+            "diab",
+            "Post coordinated diabetes program",
+            new String[]{"%E10%", "%E11%", "%E12%", "%E13%", "%E14%"}
+    ),
+    da(
+            "http://terminology.hl7.org/CodeSystem/episodeofcare-type",
+            "da",
+            "Drug and alcohol rehabilitation",
+            new String[]{"%F10%", "%F11%", "%F12%", "%F13%", "%F14%", "%F15%", "%F16%", "%F17%", "%F18%", "%F19%", "%Z71.4%", "%Z71.5%"}
+    ),
+    cacp(
+            "http://terminology.hl7.org/CodeSystem/episodeofcare-type",
+            "cacp",
+            "Community-based aged care",
+            new String[]{}
     );
 
     private final String system;
